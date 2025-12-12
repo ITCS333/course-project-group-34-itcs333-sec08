4720ea7646d19f4f5c7bf2c93e229b48c41e5644
osessa <osamaessabh@gmail.com>
2025-12-12 03:16:46 +0300

    create database.php

---
+++ src/resources/config/Database.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "web_db";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function getConnection() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
